04a913cc5d102c9aa82c2e156c1e6634c600be19
Ilyat1337 <miningilya2@gmail.com>
2020-11-30 13:37:54 +0300

    add dependency injector library

---
+++ DependencyInjector/Exceptions/ConfigurationException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DependencyInjector
+{
+    class ConfigurationException : Exception
+    {
+        private readonly string message;
+
+        public override string Message
+        {
+            get { return message; }
+        }
+
+        public ConfigurationException(string message)
+        {
+            this.message = message;
+        }
+    }
+}
